3cdead7add823a4df0e94a073334f3789c24be32
bhushan baddiya <bhushanbaddiya0@gmail.com>
2024-02-10 16:44:07 +0530

    changed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,22 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        B obj = new B();
+        String str = obj.meth();
+        System.out.println("str");
+    }
+
+
+    public static class A {
+       public  String meth(){
+           return "Invoking method from class A";
+       }
+    }
+
+    static class B extends A {
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
 }
